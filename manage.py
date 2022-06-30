5ba4d0ecfcb1254fc9e19fd2dfb3b79c1e50961f
AsqaraliyevFakhriyor <asqaraliyev01@gmail.com>
2022-06-30 18:50:15 +0500

    removed flask script

--- manage.py
+++ manage.py
@@ -1,3 +1,9 @@
-from app import db
+from locale import currency
+from sqlite3 import dbapi2
+from models import db
+from flask import current_app
+
+app = current_app()
+db.init_app(app)
 
 db.create_all()
