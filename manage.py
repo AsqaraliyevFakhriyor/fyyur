7d2fd2179f77e641080f26f5c82bbe3d81adf7ef
AsqaraliyevFakhriyor <asqaraliyev01@gmail.com>
2022-06-30 18:56:42 +0500

    bug-fix

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from sqlite3 import dbapi2
 from models import db
 from flask import current_app
 
-app = current_app()
+app = current_app
 db.init_app(app)
 with app.app_context():
     db.create_all()
